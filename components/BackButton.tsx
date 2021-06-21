567da183b889ee0354c11fa83774d35f3ad9f7ec
dave <dave@davecode.me>
2021-06-21 13:13:12 -0400

    music page and back button

---
+++ components/BackButton.tsx
@@ -0,0 +1,14 @@
+import clsx from 'clsx';
+import Link from 'next/link';
+
+import c from './BackButton.module.scss'
+
+interface BackButtonProps {
+    inverted?: boolean;
+}
+
+export default function BackButton({ inverted }: BackButtonProps) {
+    return <nav className={clsx(c.root, inverted && c.inverted)}>
+        <Link href='/'>go home</Link>
+    </nav>
+}
