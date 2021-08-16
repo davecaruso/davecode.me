7b47eefce4cc145cef0c8915fce65c6efc35b6c6
dave caruso <dave@davecode.me>
2021-08-15 21:41:51 -0400

    add games

---
+++ public/2048/precache-manifest.7987f8d68b74c193a1ec126798975295.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ced3a6697ce6ae2b9f0558505346c23",
+    "url": "/2048/index.html"
+  },
+  {
+    "revision": "8bef133d32940a5930fb",
+    "url": "/2048/static/css/main.d2ae2b4b.chunk.css"
+  },
+  {
+    "revision": "adebd3d3ae6410cbd0e0",
+    "url": "/2048/static/js/2.96b32859.chunk.js"
+  },
+  {
+    "revision": "8bef133d32940a5930fb",
+    "url": "/2048/static/js/main.2390f1eb.chunk.js"
+  },
+  {
+    "revision": "e387bff5e0b18c0491fd",
+    "url": "/2048/static/js/runtime~main.f72a3c6a.js"
+  },
+  {
+    "revision": "329d2734c01860f63d8d2db6f78a4d47",
+    "url": "/2048/static/media/background.329d2734.svg"
+  }
+]);
